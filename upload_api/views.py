a7c1d41bbbdbbbb1a84d46bfe1c7ce7eff872945
rocky-hasan <rakibulhasancblock1a@gmail.com>
2023-12-21 18:27:27 +0600

    views work done

--- upload_api/views.py
+++ upload_api/views.py
@@ -4,8 +4,8 @@ from upload_api.serializers import ProfileSerializers
 from rest_framework.views import APIView
 from rest_framework import status
 from upload_api.models import Profile
+from upload_api.forms import ProfileForm
 # Create your views here.
-from .forms import ProfileForm
 
 class ProfileApiView(APIView):
     def get(self, request, format=None):
@@ -19,3 +19,21 @@ class ProfileApiView(APIView):
             form.save()
             return render(request, 'uploadsuccess.html', {'msg': 'File Upload Successfully'})
         return Response({'status': 'Error', 'errors': form.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+# class ProfileApiView(APIView):
+#     def post(self,request,format=None):
+#         serializer=ProfileSerializers(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({'msg':'File Upload Successfully','status':'Success','candidate':serializer.data},status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors)
+#     def get(self,request,format=None):
+#         candidates=Profile.objects.all()
+#         serializer=ProfileSerializers(candidates, many=True)
+#         return Response({'status':'Success','candidate':serializer.data},status=status.HTTP_200_OK)
+
